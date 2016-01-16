19876bb8d3e135132db1b70ee4ba6b218396bc52
Gordon Wells <gordon.wells@gmail.com>
2016-01-17 01:58:33 +0200

    Update readme to explain ariadne

--- rc.py
+++ rc.py
@@ -1,4 +1,3 @@
-# from percol.finder import FinderMultiQueryMigemo, FinderMultiQueryRegex
 percol.view.CANDIDATES_LINE_BASIC    = ("on_default", "default")
 percol.view.CANDIDATES_LINE_SELECTED = ("underline", "on_blue", "white","bold")
 percol.view.CANDIDATES_LINE_MARKED   = ("bold", "on_cyan", "black")

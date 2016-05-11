2d9679e8fe10cb83a2e14631b3d4f5e6f8a0588f
Gordon Wells <gordon.wells@gmail.com>
2016-05-12 01:19:01 +0200

    Injection into command line buffer works now
    Thanks to browsing fzf code

--- config.fish
+++ config.fish
@@ -1,17 +1,10 @@
 # Path to Oh My Fish install.
 set -g OMF_PATH "/home/gordon/.local/share/omf"
-
-# Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG "/home/gordon/.config/omf"
-
-# Load oh-my-fish configuration.
-# source $OMF_PATH/init.fish
-
 function fish_log --on-event fish_preexec
-  echo $history[1]" ### "(date)" , "(pwd) >> ~/.fish_log
+  echo $history[1]" ### "(date +'%F %H:%M')" , "(pwd) >> ~/.fish_log
 end
 
-function browse_log
+function browse_fish_history
   set PERCOL "$HOME/.config/fish/functions/ariadne/bin/percol"
   set RCFILE "$HOME/.config/fish/functions/ariadne/rc.py"
   set SEP " <> "
@@ -23,5 +16,16 @@ function browse_log
         ORS="\n"; \
         print $1; \
     }' ~/.fish_log  | eval $PERCOL --reverse --rcfile=$RCFILE
+end
+
+if [ -z "$TMPDIR" ]
+    set -g TMPDIR /tmp
+end
+
+function store_fish_history
+	browse_fish_history > $TMPDIR/fish.result 
+	and commandline (cat $TMPDIR/fish.result)
+	commandline -f repaint
+    rm -f $TMPDIR/fish.result
 end
 

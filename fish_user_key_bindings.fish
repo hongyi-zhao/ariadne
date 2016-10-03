caa69967f5bfa068a93f76da9b147ceca7d3edbe
Gordon Wells <gordon.wells@gmail.com>
2016-10-03 11:33:12 +0200

    fix git merge bs for fish

--- fish_user_key_bindings.fish
+++ fish_user_key_bindings.fish
@@ -1,8 +1,4 @@
 function fish_user_key_bindings
-<<<<<<< HEAD
 	bind \cR ariadne
-=======
-	bind \cR inject_fish_history
->>>>>>> 654aac459a3ea30d2414ef8b5072aa5887c168de
 end
 
--- store_fish_history.fish
+++
@@ -1,6 +0,0 @@
-function ariadne
-	browse_fish_history > $TMPDIR/fish.result 
-	and commandline (cat $TMPDIR/fish.result)
-	commandline -f repaint
-    rm -f $TMPDIR/fish.result
-end

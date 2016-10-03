ccf3103361025432f2ef668fc634d9b8bd712a94
Gordon Wells <gordon.wells@gmail.com>
2016-10-03 12:01:02 +0200

    Remove AWKness from fish configs
    Must still see to reading SEP value in python and all shell compenents

--- config.fish
+++ config.fish
@@ -1,6 +1,5 @@
-# Path to Oh My Fish install.
 function fish_log --on-event fish_preexec
-  echo $history[1]" ### "(date +'%F %H:%M')" , "(pwd) >> ~/.fish_log
+  echo $history[1]" ### "(date +'%F %H:%M')" ,,"(whoami)","(hostname)" ,"(pwd) >> ~/.fish_log
 end
 
 if [ -z "$TMPDIR" ]

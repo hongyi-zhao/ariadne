ae24c21fb9b09753ab99bcfebe79ab8f797b4495
Gordon Wells <gordon@gmail.com>
2018-03-06 23:15:02 +0200

    add exit status recording for fish

--- config.fish
+++ config.fish
@@ -1,5 +1,5 @@
 function fish_log --on-event fish_preexec
-  echo $history[1]" ### "(date +'%F %H:%M')" ,,"(whoami)","(hostname)" ,"(pwd) >> ~/.fish_log
+  echo $history[1]" ### "(date +'%F %H:%M')" ,,"(whoami)","(hostname)" ,"(pwd)" ,"(echo $status) >> ~/.fish_log
 end
 
 if [ -z "$TMPDIR" ]

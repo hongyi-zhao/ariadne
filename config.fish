d46b4be59b8c28e6518a431e1695bb187e711ae5
Gordon Wells <gordon@gmail.com>
2020-04-06 21:18:56 +0200

    modified fish config to prevent logging consecutive duplicates

--- config.fish
+++ config.fish
@@ -1,8 +1,10 @@
 function fish_log --on-event fish_preexec
-  echo $history[1]" ### "(date +'%F %H:%M')" ,,"(whoami)","(hostname)" ,"(pwd)" ,"(echo $status) >> ~/.fish_log
-  # comment out if not saving to master log (e.g. symlink to cloud file) for multiple pcs:
-  echo $history[1]" ### "(date +'%F %H:%M')" ,,"(whoami)","(hostname)" ,"(pwd)" ,"(echo $status) >> ~/.fish_master_log
+    set h1 (tail -n 1 .fish_log | awk 'BEGIN {FS=" ### "}  {print $1}')
+    set h2 $history[1]
 
-  # TODO: add check for duplicate commands
+    if [ $h1 != $h2 ] # check for consecutive duplicates
+        echo $history[1]" ### "(date +'%F %H:%M')" ,,"(whoami)","(hostname)" ,"(pwd)" ,"(echo $status) >> ~/.fish_log
+        # comment out if not saving to master log (e.g. symlink to cloud file) for multiple pcs:
+        echo $history[1]" ### "(date +'%F %H:%M')" ,,"(whoami)","(hostname)" ,"(pwd)" ,"(echo $status) >> ~/.fish_master_log
+    end
 end
-
